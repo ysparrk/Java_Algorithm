87738b47f46540742d1ba552cb54f9180a99984d
ysparrk <youngseo0703@gmail.com>
2023-03-08 17:43:00 +0900

    230308

---
+++ src/Main_2588.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main_2588 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 한 줄을 읽고, int로 변경
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+
+}
