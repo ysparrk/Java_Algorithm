7954935a2b9174f52905d6c7206cfe700d956c33
ysparrk <youngseo0703@gmail.com>
2023-03-16 17:59:31 +0900

    230316

---
+++ src/Main_3052.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class Main_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+    }
+}
